993ff6242d05c78dd32ee6f072015f0db13ff036
Xander Salathe <xsalathe@gmail.com>
2024-10-19 09:54:28 +1100

    fix: redis dbclient init

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,19 +4,22 @@ import (
 	"context"
 	"fmt"
 	"github.com/go-redis/redis/v8"
-	"gochop-it/internal/utils"
 	"log"
 	"net/http"
+	"gochop-it/internal/utils"
+	"gochop-it/internal/repository"
 )
+
+var ctx = context.Background()
 
 func main() {
 	// Redis setup
-	rdb := redis.NewClient(&redis.Options{
+	dbClient := redis.NewClient(&redis.Options{
 		Addr: "redis:6379", // Redis service hostname in Docker Compose
 	})
 
 	// Test Redis connection
-	_, err := rdb.Ping(context.Background()).Result()
+	_, err := dbClient.Ping(context.Background()).Result()
 	if err != nil {
 		log.Fatalf("Could not connect to Redis: %v", err)
 	}
@@ -30,16 +33,20 @@ func main() {
 	http.HandleFunc("/shorten", func(writer http.ResponseWriter, req *http.Request) {
 		// Get the URL to shorten from the request
 		url := req.FormValue("url")
+
 		// Close the body when done
 		fmt.Println("Payload: ", url)
+
 		// Shorten the URL
 		shortURL := utils.GetShortCode()
 		fullShortURL := fmt.Sprintf("http://localhost:8080/r/%s",
 			shortURL)
-		fmt.Printf("Generated short URL: %s\n", fullShortURL)
 		// Generated short URL
+		fmt.Printf("Generated short URL: %s\n", fullShortURL)
 		fmt.Printf("Generated short URL: %s\n", shortURL) // Log to console
-		// @TODO: Store {shortcode: url} in Redis
+
+		// Set the key in Redis 
+		repository.SetKey(&ctx, dbClient, shortURL, url, 0)  
 		// @TODO return the shortened URL in the UI
 	})
 
