3a6add8329a2a23575fc2f6411a2f68164eb3d59
Xander Salathe <xsalathe@gmail.com>
2024-10-19 09:49:20 +1100

    feat: update redis functions

--- internal/repository/redis_repo.go
+++ internal/repository/redis_repo.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-redis/redis/v8"
 )
 
@@ -18,11 +19,27 @@ func NewRedisRepo() *RedisRepo {
 }
 
 // takes a short URL and its corresponding original URL as input, and saves the mapping to Redis
-func (r *RedisRepo) SaveURL(shortURL, originalURL string) error {
-	return r.client.Set(context.Background(), shortURL, originalURL, 0).Err()
+func SetKey(ctx *context.Context, rdb *redis.Client, key string, value string, ttl int) {
+	// We set the key value pair in Redis, we use the context defined in main by reference and a TTL of 0 (no expiration)
+	fmt.Println("Setting key", key, "to", value, "in Redis")
+
+	rdb.Set(*ctx, key, value, 0)
+
+	fmt.Println("The key", key, "has been set to", value, "successfully")
 }
 
 // retrieves and returns the original URL corresponding to the given short URL from Redis
-func (r *RedisRepo) GetURL(shortURL string) (string, error) {
-	return r.client.Get(context.Background(), shortURL).Result()
+func GetLongURL(ctx *context.Context, rdb *redis.Client, shortURL string) (string, error) {
+
+	longURL, err := rdb.Get(*ctx, shortURL).Result()
+
+	if err == redis.Nil {
+		return "", fmt.Errorf("short URL not found")
+
+	} else if err != nil {
+		return "", fmt.Errorf("failed to retrieve from Redis: %v", err)
+
+	}
+
+	return longURL, nil
 }
