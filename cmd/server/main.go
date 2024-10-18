5290c668f2847d6c81e9946270bf7c7805f5d752
Xander Salathe <xsalathe@gmail.com>
2024-10-18 19:26:36 +1100

    feat: docker init

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/go-redis/redis/v8"
+	"log"
 	"net/http"
 )
 
 func main() {
+	// Redis setup
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "redis:6379", // Redis service hostname in Docker Compose
+	})
+
+	// Test Redis connection
+	_, err := rdb.Ping(context.Background()).Result()
+	if err != nil {
+		log.Fatalf("Could not connect to Redis: %v", err)
+	}
+	fmt.Println("Connected to Redis!")
+
 	http.HandleFunc("/", func(writer http.ResponseWriter, req *http.Request) {
 		fmt.Fprintln(writer, "hello world")
 	})
