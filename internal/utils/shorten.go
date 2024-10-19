1f4109e0cb17067f71ed8cca71afbc4ef29b3cbd
Xander Salathe <xsalathe@gmail.com>
2024-10-19 14:52:39 +1100

    fix: formatting

--- internal/utils/shorten.go
+++ internal/utils/shorten.go
@@ -17,7 +17,7 @@ func GetShortCode() string {
 	ts_bytes := []byte(fmt.Sprintf("%d", ts))
 	key := base64.StdEncoding.EncodeToString(ts_bytes)
 	fmt.Println("Key: ", key)
-	
+
 	// We remove the last two chars since they are usuall always equal signs (==)
 	key = key[:len(key)-2]
 	// We return the last chars after 16 chars, these are almost always different
