bc8cf36fd5e80bb455b2e6d0a9536c6bd9cd7474
Xander Salathe <xsalathe@gmail.com>
2024-10-26 17:06:02 +1100

    fix: remove custom mongo app entry point

--- mongo-setup.sh
+++ mongo-setup.sh
@@ -3,6 +3,3 @@
 # Replace placeholders in js file with actual environment variable values
 sed -i "s/MONGO_APP_USERNAME/${MONGO_APP_USERNAME}/g" /docker-entrypoint-initdb.d/mongo-user-init.js
 sed -i "s/MONGO_APP_PASSWORD/${MONGO_APP_PASSWORD}/g" /docker-entrypoint-initdb.d/mongo-user-init.js
-
-# Start MongoDB with the modified init script
-exec "$@"
