6c89db853cffd9cfe6b4c28981939c4532a4762b
Xander Salathe <xsalathe@gmail.com>
2024-10-26 16:23:29 +1100

    fix: inject mongo variable values with bash script

--- mongo-user-init.js
+++ mongo-user-init.js
@@ -2,8 +2,8 @@ db = db.getSiblingDB("url_shortener");
 
 // Create application user with readWrite and dbAdmin roles on the url_shortener database
 db.createUser({
-    user: "${MONGO_APP_USERNAME}",
-    pwd: "${MONGO_APP_PASSWORD}",
+    user: "MONGO_APP_USERNAME",
+    pwd: "MONGO_APP_PASSWORD",
     roles: [
         {
             role: "readWrite",
